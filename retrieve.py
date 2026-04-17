19f152f7b4df39690c12026ae81ef510219de982
EWARS2 <jefmacho99@gmail.com>
2026-04-17 14:53:15 -0500

    add: retrieval tool works

---
+++ retrieve.py
@@ -0,0 +1,16 @@
+import socket
+import os
+import subprocess
+
+hostname = socket.gethostname()
+print("Hostname: " + hostname)
+output = None
+
+if os.name == "nt":
+    print("Windows system...")
+    output = subprocess.check_output(['msinfo32', '/nfo', f'{hostname}.nfo'], text=True)
+elif os.name == "posix":
+    print("POSIX system...")
+    output = subprocess.check_output(['sudo', 'lshw', '>', f'{hostname}.xml'], text=True)
+
+print(output)
