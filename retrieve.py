20d05777018e3e37c0981c742caf903a915b6323
EWARS2 <jefmacho99@gmail.com>
2026-04-17 15:22:48 -0500

    fix: cleanup

--- retrieve.py
+++ retrieve.py
@@ -4,13 +4,11 @@ import subprocess
 
 hostname = socket.gethostname()
 print("Hostname: " + hostname)
-output = None
 
 if os.name == "nt":
     print("Windows system...")
     output = subprocess.check_output(['msinfo32', '/nfo', f'{hostname}.nfo'], text=True)
+    print(output)
 elif os.name == "posix":
     print("POSIX system...")
     subprocess.run(f"sudo lshw -xml > {hostname}.xml", shell=True, check=True)
-
-print(output)
